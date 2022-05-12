85505f6d4b035dd1c7bec6039e2d681bdeb9cb61
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:04 -0300

    chore: re index discordnim

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."discord-develop".dir   = "develop";
+  inputs."discord-develop".owner = "nim-nix-pkgs";
+  inputs."discord-develop".ref   = "master";
+  inputs."discord-develop".repo  = "discord";
+  inputs."discord-develop".type  = "github";
+  inputs."discord-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."discord-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."discord-master".dir   = "master";
+  inputs."discord-master".owner = "nim-nix-pkgs";
+  inputs."discord-master".ref   = "master";
+  inputs."discord-master".repo  = "discord";
+  inputs."discord-master".type  = "github";
+  inputs."discord-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."discord-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
